d2ef1044b4ec1a6e3e2f7886423ce7e1292e708e
Alexandre Piel <alexandre.piel@gmail.com>
2019-01-28 22:34:17 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+
+import { sync } from 'glob';
+import { execSync } from 'child_process';
+
+const files: string[] = sync('/home/alex/dev/test/e2e/eg-app/**/*.test.e2e.js');
+
+files.forEach((file: string) => {
+    // console.log('load file', file);
+    // const load = require(file);
+    // console.log('load', load);
+    const result = execSync(`jest ${file}`);
+    console.log('result', result);
+});
