cb39232117b46bd57f7f0f40146cff68f79e2b36
Alexandre Piel <alexandre.piel@gmail.com>
2019-01-30 19:16:35 +0100

    save data

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,25 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
 const util_1 = require("util");
+const path_1 = require("path");
+const mkdirp = require("mkdirp");
 const defaultBaseUrl = process.env.BASE_URL || 'http://localhost:3000';
 function loadUrls(dataUrls, baseUrl = defaultBaseUrl) {
-    return util_1.promisify(fs_1.writeFile)(`${__dirname}/../data/yo.txt`, JSON.stringify({ dataUrls, baseUrl }, null, 4));
+    const actions = dataUrls.map((dataUrl, index) => __awaiter(this, void 0, void 0, function* () {
+        const file = `${__dirname}/../data/${dataUrl.pathUrl}`;
+        yield util_1.promisify(mkdirp)(path_1.dirname(file));
+        yield util_1.promisify(fs_1.writeFile)(`${file}.data`, JSON.stringify({ dataUrl, baseUrl }, null, 4));
+    }));
+    return Promise.all(actions);
 }
 exports.loadUrls = loadUrls;
 //# sourceMappingURL=index.js.map
--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,7 @@
 import { writeFile } from 'fs';
 import { promisify } from 'util';
+import { dirname } from 'path';
+import * as mkdirp from 'mkdirp';
 
 const defaultBaseUrl: string = process.env.BASE_URL || 'http://localhost:3000';
 
@@ -11,9 +13,19 @@ export interface DataUrl {
 export function loadUrls(
     dataUrls: DataUrl[],
     baseUrl: string = defaultBaseUrl,
-): Promise<void> {
-    return promisify(writeFile)(
-        `${__dirname}/../data/yo.txt`,
-        JSON.stringify({ dataUrls, baseUrl }, null, 4),
+): Promise<void[]> {
+    const actions = dataUrls.map(
+        async (dataUrl: DataUrl, index: number) => {
+            await saveData(dataUrl, baseUrl);
+        });
+    return Promise.all(actions);
+}
+
+async function saveData(dataUrl: DataUrl, baseUrl: string) {
+    const file = `${__dirname}/../data/${dataUrl.pathUrl}`;
+    await promisify(mkdirp)(dirname(file));
+    await promisify(writeFile)(
+        `${file}.data`,
+        JSON.stringify({ dataUrl, baseUrl }, null, 4),
     );
 }
